8526fc76a9e70d76e5478cc3c80c048e390308d7
Nuria Mohamed Hussein <nuria.hussein@student.moringaschool.com>
2024-12-08 00:07:45 +0300

    commit all

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(toDo = 'roller-skate') {
+    return `This Saturday, I want to ${toDo}!`;
+}
+console.log(saturdayFun());
+console.log(saturdayFun('bathe my dog'));
+
+const mondayWork = function (activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+console.log(mondayWork());
+console.log(mondayWork('work from home.'));
+
+function wrapAdjective(flair ="*"){
+    return function(adjective = "a hard worker"){
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+console.log(wrapAdjective("||")("a dedicated programmer"));
